dc12b9bf6a7b66cdbe0d9ff734de271d16c14fcf
Deepak <deepakgithub4@gmail.com>
2024-09-21 22:34:46 +0530

    datepicker2

--- src/tests/datePicker.spec.ts
+++ src/tests/datePicker.spec.ts
@@ -1,7 +1,13 @@
 import {test} from '@playwright/test'
 
-test('datePicker',async({page})=> {
+test.beforeEach(async( { page }) => {
     await page.goto("https://testautomationpractice.blogspot.com/");
+})
+//-----------------------------------------------------------------------------
+
+
+
+test('while loop datePicker',async({page})=> {
 
     const year = "2023"
     const month = "February"
@@ -28,7 +34,44 @@ test('datePicker',async({page})=> {
             break;
         }
     }
+})
 
-    await page.waitForTimeout(2000)
-    await page.close()
+test('without for loop datePicker',async({page})=> {
+
+    const year = "2023"
+    const month = "February"
+    const date = "20"
+    
+    await page.click("#datepicker")
+
+    while(true){
+        const currentYear = await page.locator('.ui-datepicker-year').textContent();
+        const currentMonth = await page.locator('.ui-datepicker-month').textContent();
+
+        if(currentYear == year && currentMonth == month){
+            break;
+        }
+        await page.locator('[title="Prev"]').click()
+    }
+
+    await page.click(`//a[@class='ui-state-default'][text()='${date}']`)
+
+})
+
+test('simplest way to datepickerr',async({page})=> {
+
+await page.fill('#datepicker','03/15/2023')
+
+})
+
+
+
+
+//-------------------------------------------
+test.afterEach(async( { page }) => {
+    await page.waitForTimeout(1000)
+})
+
+test.afterAll(async( { page }) => {
+    await page.close();
 })
