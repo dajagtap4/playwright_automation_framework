b36e236adf41729273bcb43d6d3dc1d200be0db8
Deepak <deepakgithub4@gmail.com>
2024-09-23 09:17:28 +0530

    loginMultiElement

--- src/tests/LocatingMultipleElements.spec.ts
+++ src/tests/LocatingMultipleElements.spec.ts
@@ -4,12 +4,15 @@ test('multilocators',async({page})=>{
     await page.goto("https://www.demoblaze.com/index.html");
 
     const links = await page.$$('a');
+
     for(const link of links ){
         const linkText = await link.textContent();
-        console.log(linkText);
+        console.log(link);
     }
 
-    const products = await page.$$("//div[@id='tbodyid']//h4/a")
+   
+    await page.waitForSelector("//div[@id='tbodyid']//h4/a")
+    const products = await page.$$("//div[@id='tbodyid']//div//h4/a")
     for(const product of products ){
         const productName = await product.textContent();
         console.log(productName);
