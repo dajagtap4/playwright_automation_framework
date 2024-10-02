86faa2517e8062c27dd1c9718c604f4a9996c9c6
dajagtap4 <deepakgithub4@gmail.com>
2024-10-02 20:39:01 +0530

    Update uploadFile.spec.ts

--- src/tests/uploadFile.spec.ts
+++ src/tests/uploadFile.spec.ts
@@ -4,9 +4,11 @@ test('uploadfile', async ({ page }) => {
 
     await page.goto('https://demo.automationtesting.in/FileUpload.html')
     await page.waitForTimeout(1000)
-    
-    await page.locator('#input-4').setInputFiles('src/tests/uploadFiles/Java_Playwright_Adreena.docx')
 
+    //----------------------------------------------------------------------------------------------------
+    await page.locator('#input-4').setInputFiles('src/tests/uploadFiles/Java_Playwright_Adreena.docx')
+    //----------------------------------------------------------------------------------------------------
+    
     await page.waitForTimeout(1000)
     await page.close()
 })
