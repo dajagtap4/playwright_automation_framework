5faf3d5a455e49de02574eb5d9da2b2356798f68
dajagtap4 <deepakgithub4@gmail.com>
2024-10-03 22:04:01 +0530

    Update mouseHover.spec.ts

--- src/tests/mouseHover.spec.ts
+++ src/tests/mouseHover.spec.ts
@@ -7,11 +7,6 @@ test("mouse",async ({page}) => {
     const desktops = await page.locator("//nav[@id='menu']/div[2]/ul/li[1]/a[1]")
     const mac = await page.locator("//nav[@id='menu']/div[2]/ul/li[1]/div[1]/div/ul/li[2]/a")
 
-    await page.waitForTimeout(500)
     await desktops.hover()
-    await page.waitForTimeout(500)
     await mac.hover()
-
-    await page.waitForTimeout(1000)
-    await page.close()
 })
