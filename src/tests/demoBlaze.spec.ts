1990a8ffe798a68cf90b47730b5a8b3011ad701e
dajagtap4 <deepakgithub4@gmail.com>
2024-10-05 19:39:31 +0530

    Update demoBlaze.spec.ts

--- src/tests/demoBlaze.spec.ts
+++ src/tests/demoBlaze.spec.ts
@@ -1,9 +1,10 @@
-import {test} from '@playwright/test'
-import { loginPage } from '../../demoblaze-pom/loginPgae'
-import { homePage } from '../../demoblaze-pom/homePage'
-import { logoutPage } from '../../demoblaze-pom/logoutPage'
-import { cartPage } from '../../demoblaze-pom/cartPage'
+import {test, Locator, expect} from '@playwright/test'
 
+import { loginPage } from '../demoblaze-pom/loginPage'
+import { homePage } from '../demoblaze-pom/homePage'
+import { logoutPage } from '../demoblaze-pom/logoutPage'
+import { cartPage } from '../demoblaze-pom/cartPage'
+import { placeOdrForm } from '../demoblaze-pom/placeOdrForm'
 
 test.beforeEach(async( {page} ) => {
     await page.goto("https://www.demoblaze.com/index.html")
@@ -31,7 +32,6 @@ test('TC:002 - Check user can select product from item list',async( {page} ) => 
 test('TC:003 - Check user can add product in cart and delete it',async( {page} ) => {
    
     const login = new loginPage(page)
-    const logout = new logoutPage(page)
     const home = new homePage(page)
     const cart = new cartPage(page)
     
@@ -44,6 +44,27 @@ test('TC:003 - Check user can add product in cart and delete it',async( {page} )
     await cart.deleteProductFromCart()
 })
 
-test.afterAll(async( { page }) => {
+test('TC:004 - Check user can place order and verify confirmation massage',async( {page} ) => {
+   
+    const login = new loginPage(page)
+    const home = new homePage(page)
+    const cart = new cartPage(page)
+    const orderPlcae = new placeOdrForm(page)
+    const logout = new logoutPage(page)
+
+    await login.loginToPage();
+    await login.checkLoginFunctionality()
+    await home.selectProductOnHomePage()
+    await cart.checkAddToCartbuttonVisible()
+    await cart.addToCartButton()
+    await cart.cartUI()
+    await cart.placeOrder()
+    await orderPlcae.fillPlaceOrderForm()
+    await orderPlcae.checkThankYouForYourPurchase()
+    await orderPlcae.oKButton()
+    await logout.logoutFromPage()
+})
+
+test.afterEach(async( { page }) => {
     await page.close();
 })
