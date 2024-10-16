3113c60f9d82c9ebca25afd5220041397c40bd8c
dajagtap4 <deepakgithub4@gmail.com>
2024-10-16 14:33:06 +0530

    Update keyboardActions.spec.ts

--- src/tests/keyboardActions.spec.ts
+++ src/tests/keyboardActions.spec.ts
@@ -1,3 +1,11 @@
+import { test } from "@playwright/test"
+
+test("keyboardActions",async ({page}) => {
+ 
+    await page.goto("https://gotranscript.com/text-compare")
+
+    await page.locator('[name="text1"]').fill("Deepak jagtap")
+
     await page.keyboard.press('Control+A')
     await page.keyboard.press('Control+C')
 
@@ -5,3 +13,4 @@
     await page.keyboard.up('Tab')
 
     await page.keyboard.press('Control+V')
+});
