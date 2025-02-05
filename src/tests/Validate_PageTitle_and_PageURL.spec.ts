f5fcabea0e0cc68207a6e1580b06f6a115ade939
Deepak Jagtap <deepakgithub4@gmail.com>
2025-02-05 13:04:16 +0530

    Update Validate_PageTitle_and_PageURL.spec.ts

--- src/tests/Validate_PageTitle_and_PageURL.spec.ts
+++ src/tests/Validate_PageTitle_and_PageURL.spec.ts
@@ -3,10 +3,10 @@ import { expect, test } from "@playwright/test";
 test('Home page',async({page})=>{
     await page.goto("https://www.demoblaze.com/index.html");
 
-    const pageTitle = page.title();
+    const pageTitle = await page.title();
     console.log('page title is ', pageTitle)
 
-    const pageURL=page.url();
+    const pageURL= await page.url();
     console.log('page url is ', pageURL)
 
     await expect(page).toHaveURL('https://www.demoblaze.com/index.html');
