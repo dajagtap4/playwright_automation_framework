0a9c7c2b64e86ae829a7497c00a4c54d7dc60f2e
Deepak <deepakgithub4@gmail.com>
2025-03-14 15:16:43 +0530

    assertions file updated

--- playwright_javascript_InterviewQs/tests/Assertions.spec.js
+++ playwright_javascript_InterviewQs/tests/Assertions.spec.js
@@ -1,38 +1,38 @@
-import { test, expect, page, chromium } from '@playwright/test'
+import { test, expect, chromium } from '@playwright/test'
+import { describe } from 'node:test'
 
-test('TC 001, Check if an element is visible', async ({page}) => {
-    await page.goto("https://testautomationpractice.blogspot.com/")
-    await expect(page.locator('//*[@id="Blog1"]/div[1]/div/div/div/div/h3/a')).toBeVisible();
-})
+test.describe('assertions in playwright with diff. methods', () => {
 
-test('TC 002, Check if an element contains text', async ({page}) => {
-    await page.goto("https://testautomationpractice.blogspot.com/")
-    await expect(page.locator('//*[@id="Blog1"]/div[1]/div/div/div/div/h3/a')).toHaveText('GUI Elements');
-})
+    test.beforeEach(async ({ page }) => {
+        await page.goto("https://testautomationpractice.blogspot.com/");
+    })
+    test('TC 001, Check if an element is visible', async ({ page }) => {
+        await expect(page.locator('//*[@id="Blog1"]/div[1]/div/div/div/div/h3/a')).toBeVisible();
+    })
 
-test('TC 003, Check if an element contains a specific attribute', async ({page}) => {
-    await page.goto("https://testautomationpractice.blogspot.com/")
-    await expect(page.locator('#name')).toHaveAttribute('placeholder','Enter Name');
-})
+    test('TC 002, Check if an element contains text @runOnlyThis ', async ({ page }) => {
+        await expect(page.locator('//*[@id="Blog1"]/div[1]/div/div/div/div/h3/a')).toHaveText('GUI Elements');
+    })
 
-test('TC 004, Check if an element is enabled or disabled', async ({page}) => {
-    await page.goto("https://testautomationpractice.blogspot.com/")
+    test('TC 003, Check if an element contains a specific attribute', async ({ page }) => {
+        await expect(page.locator('#name')).toHaveAttribute('placeholder', 'Enter Name');
+    })
 
-    await expect(page.locator('//*[@id="confirmBtn"]')).toBeEnabled();
-    //will fail because button is not Disabled
-    await expect.soft(page.locator('//*[@id="confirmBtn"]')).toBeDisabled(); 
+    test('TC 004, Check if an element is enabled or disabled', async ({ page }) => {
+        await expect(page.locator('//*[@id="confirmBtn"]')).toBeEnabled();
+        //will fail because button is not Disabled
+        await expect.soft(page.locator('//*[@id="confirmBtn"]')).toBeDisabled();
 
-    const isEnabled  = await page.locator('//*[@id="confirmBtn"]').isEnabled();
-    const isDisabled  = await page.locator('//*[@id="confirmBtn"]').isDisabled();
-})
+        const isEnabled = await page.locator('//*[@id="confirmBtn"]').isEnabled();
+        const isDisabled = await page.locator('//*[@id="confirmBtn"]').isDisabled();
+    })
 
-test('TC 005, Check if the page title is correct', async ({page}) => {
-    await page.goto("https://testautomationpractice.blogspot.com/")
-    await expect(page).toHaveTitle('Automation Testing Practice');
-})
+    test('TC 005, Check if the page title is correct @runOnlyThis', async ({ page }) => {
+        await expect(page).toHaveTitle('Automation Testing Practice');
+    })
 
-test('TC 006, Check if a checkbox is checked', async ({page}) => {
-    await page.goto("https://testautomationpractice.blogspot.com/")
-    await page.click("#sunday");
-    await expect(page.locator('#sunday')).toBeChecked()
+    test('TC 006, Check if a checkbox is checked', async ({ page }) => {
+        await page.click("#sunday");
+        await expect(page.locator('#sunday')).toBeChecked()
+    })
 })
