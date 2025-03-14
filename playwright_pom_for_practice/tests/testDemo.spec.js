b9a012b9a37bfe4d961116562e48e2f767cd3034
Deepak Jagtap <deepakgithub4@gmail.com>
2025-03-15 02:12:34 +0530

    Update testDemo.spec.js

--- playwright_pom_for_practice/tests/testDemo.spec.js
+++ playwright_pom_for_practice/tests/testDemo.spec.js
@@ -1,5 +1,5 @@
-const { test, expect } = require('@playwright/test');
-const { practicePOM } = require('../pages/practicePOM');
+import { test, expect } from '@playwright/test';
+import { practicePOM } from '../pages/practicePOM';
 const configDatas = JSON.parse(JSON.stringify(require('../utils/configData.json')));
 const configLocators = JSON.parse(JSON.stringify(require('../utils/configLocator.json')));
 
@@ -9,23 +9,27 @@ test.describe('Practice POM Test Suite', () => {
     let practice;
 
     test.beforeAll(async ({ browser }) => {
-        // Launching browser and creating a new page
-        page = await browser.newPage();
+        page = await browser.newPage();  // Create a new page instance
         practice = new practicePOM(page); // Instantiate the POM
     });
 
     test.beforeEach(async () => {
         // Navigate to the test URL before each test
-        await page.goto(configDatas.testurl);
+        await page.goto(configDatas.testurl); // Use 'page' from beforeAll
     });
 
-    test(' the Page Title', async () => {
+    test('TC001 the Page Title', async () => {
         const title = await practice.getPageTitle();
         expect(title).toBe(configDatas.expectedTitle);
     });
 
-    test('Enter Name and Verify Input', async () => {
-        await practice.enterName(configDatas.userName); // Use the method to enter name
+    test('TC002 verigy incorrect Page Title', async () => {
+        const title = await practice.getPageTitle();
+        expect(title).not.toBe(configDatas.wrongtitle);
+    });
+
+    test('TC003 Enter Name and Verify Input', async () => {
+        await practice.enterName(configDatas.userName);  // Use 'practice' from beforeAll
         const enteredName = await page.locator(configLocators.nameInputBox).inputValue();
         expect(enteredName).toBe(configDatas.userName);
     });
