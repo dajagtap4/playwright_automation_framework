ceacf38886b41e184e5ebcade3a573ff51760d5b
Deepak <deepakgithub4@gmail.com>
2025-07-17 16:15:41 +0530

    API get post delete

---
+++ Playwright_Switch2.0/tests/APIs/api_test.spec.js
@@ -0,0 +1,61 @@
+const { test, expect } = require('@playwright/test');
+//const { request } = require('node:http');
+
+test("API GET Request",async({request})=>{
+
+    const responseGet = await request.get('https://reqres.in/api/users/2');
+
+    expect(responseGet.status()).toBe(200);
+
+    const text = await responseGet.text();
+    expect(text).toContain('Janet');
+
+    console.log(await responseGet.json());
+    
+});
+
+//this post test is not working at toBe(201); but code is correct 
+test("API POST Request",async({request})=>{
+
+    const responsePost = await request.post('https://reqres.in/api/users',{
+            json:{
+                name: "Deepak",
+                job : "QA"
+            }
+        });
+
+    expect(responsePost.status()).toBe(201);
+
+    const text = await responsePost.text();
+    expect(text).toContain('Deepak');
+
+     console.log(await responsePost.json());
+    
+});
+
+//this post test is not working at toBe(200); but code is correct 
+test("API PUT Request",async({request})=>{
+
+    const responsePut = await request.put('https://reqres.in/api/users/2',{
+            json:{
+                name: "Deepak",
+                job : "QA"
+            }
+        });
+
+    expect(responsePut.status()).toBe(200);
+
+    const text = await responsePut.text();
+    expect(text).toContain('Deepak');
+
+     console.log(await responsePut.json());
+    
+});
+
+//this post test is not working at toBe(204); but code is correct 
+test("API DELETE Request",async({request})=>{
+
+    const responseDelete = await request.delete('https://reqres.in/api/users/2');
+
+    expect(responseDelete.status()).toBe(204);
+});
---
+++ Playwright_Switch2.0/tests/basic_interviews/SloMo_VidRecord.spec.js
@@ -0,0 +1,31 @@
+import { test, expect, chromium } from '@playwright/test';
+import { dir } from 'console';
+
+test('slomo', async ({  }) => {
+
+   const browser = await chromium.launch({
+    slowMo: 500,
+    headless: false
+   });
+
+   const context = await browser.newContext({
+    recordVideo: {
+        dir: 'vodeos/',
+        size: {width: 800, height:600}
+    }
+   });
+
+   const page = await context.newPage();
+
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.locator('[data-test="logout-sidebar-link"]').click();
+
+  await context.close();
+
+})
