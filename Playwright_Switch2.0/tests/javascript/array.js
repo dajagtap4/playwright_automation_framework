030688d4dd36cd49e9a3856eb5a8a72dc3660057
dajagtap4 <deepakgithub4@gmail.com>
2025-08-07 11:49:38 +0530

    arrays

---
+++ Playwright_Switch2.0/tests/javascript/array.js
@@ -0,0 +1,25 @@
+let arr = new Array(5);
+
+	arr[0] = 2;
+	arr[1] = 4;
+	arr[3] = 5;
+
+console.log("Array elements are: ", arr);
+
+
+//JAVA
+
+// import java.util.Arrays;
+// public class arrayDemo {
+
+// public static void main(String[] args) {
+
+// 		int arr[] = new int[4];
+// 		arr[0] = 2;
+// 		arr[1] = 4;
+// 		arr[3] = 5;
+// 		System.out.println(Arrays.toString(arr));
+
+// 	}
+// }
+
