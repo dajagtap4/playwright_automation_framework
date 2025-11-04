c478db0f5864600dbb0448114e4472805cd46721
Deepak <deepakgithub4@gmail.com>
2025-11-04 22:37:35 +0530

    updated readExcelFile.js

--- Playwright_Switch2.0/tests/excelJS/step-by-step/readExcelFile.js
+++ Playwright_Switch2.0/tests/excelJS/step-by-step/readExcelFile.js
@@ -1,12 +1,22 @@
 // read data from excel file and print all cell values
-// run with command -> node readExcelFile.js
+// download excel file from below link and rename it to "excelDownloadTest.xlsx"
+// https://rahulshettyacademy.com/upload-download-test/index.html
+// run this js file with command -> node readExcelFile.js
 // ==========================================================
+
 
 const ExcelJs = require('exceljs');
 
 async function readExcelFileDemo(){
     
+    // ExcelJs → It is library which helps to work with Excel files (.xlsx).
+    // Workbook → whole Excel file (it can have many sheets inside).
+    // new ExcelJs.Workbook() → creates new empty Excel file in memory.
+    
     const workbook = new ExcelJs.Workbook();
+
+    //workbook.xlsx.readFile(...) → This tells ExcelJs to open an existing Excel file.
+    
     await workbook.xlsx.readFile('C:/Users/Deepak/Downloads/excelDownloadTest.xlsx');
     const worksheet = workbook.getWorksheet('Sheet2');
 
@@ -16,6 +26,7 @@ async function readExcelFileDemo(){
             //each cell value
             console.log(cell.value);
         });
+        
     });
 }
 readExcelFileDemo();
