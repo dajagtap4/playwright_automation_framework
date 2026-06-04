36ace8fe4a598e0f7398428b79f81da10f4c4c27
Deepak Jagtap <deepakgithub4@gmail.com>
2026-06-04 18:17:12 +0530

    Update HomePage.ts

--- Playwright_Framework/pages/HomePage.ts
+++ Playwright_Framework/pages/HomePage.ts
@@ -4,16 +4,29 @@ export class HomePage {
 
     readonly page: Page;
     readonly successMessage: Locator;
+    readonly logoutButton: Locator; 
 
     constructor(page: Page) {
         this.page = page;
 
         this.successMessage =
             page.locator('h1.post-title');
+
+        this.logoutButton = 
+            page.locator('a', { hasText: 'Log out' }); 
     }
 
     async verifySuccessfulLogin() {
         await expect(this.successMessage)
             .toHaveText('Logged In Successfully');
     }
+
+    async logout() {                                                                               
+      await this.logoutButton.click();                      
+  } 
+
+    async verifyLogout() {                                                                         
+      await expect(this.page).toHaveURL(/practice-test-login/);                                  
+      await expect(this.page.locator('#username')).toBeVisible();                                
+  } 
 }
